d30bd1a1c2b812e1b83c5c7a7eda52db095f6445
roksana-barna <roksanabarna1803@gmail.com>
2024-05-17 03:12:59 +0600

    foot

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,15 +7,16 @@ const Banner = () => {
   return (
     <div>
       <div className="bg-black text-gradient   text-white p-8 text-center">
-        <h1 className="lg:text-5xl md:text-3xl sm:text-3xl  font-semibold mb-2">Maximize Your Potential with Our Soft </h1>
-        <Slide triggerOnce>
-        <h1 className="lg:text-4xl sm:text-3xl font-bold mb-2 mt-6" style={{ background: 'linear-gradient(45deg, #2CD4F8,#9747FF,)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>
-        Skills Courses!</h1>
+        <h1 className="lg:text-6xl md:text-3xl sm:text-3xl  font-bold mb-2 lg:mt-20">Maximize Your Potential with Our Soft </h1>
+         <Slide triggerOnce>
+        <h1 className="lg:text-6xl sm:text-3xl font-bold mb-2 mt-6" style={{ background: 'linear-gradient(45deg, #2CD4F8,#9747FF)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>
+        Skills Courses!
+        </h1>
         </Slide>
-        <p className='mb-3 font-thin text-md mt-6'>Discover the key to unlocking your full potential through our comprehensive soft skills courses. Elevate your<br/> career prospects, enhance personal growth, and excel in every aspect of life. </p>
+        <p className='mb-3 font-thin text-lg mt-6'>Discover the key to unlocking your full potential through our comprehensive soft skills courses. Elevate your<br/> career prospects, enhance personal growth, and excel in every aspect of life. </p>
         <div>
           <Link
-          className=" border-gradient inline-block text-md mt-8   font-thin text-white pt-4 px-6  btn border rounded-full transition duration-300 "
+          className=" border-gradient inline-block text-md mt-8   font-thin text-white pt-4 px-10  btn border rounded-full transition duration-300 "
         >Get Started
         </Link>
         </div>
